c46a4c83dcd2f63d1c6b7d99cbb5ae1af386b749
lakamourkhalil-hub <lakamourkhalil@gmail.com>
2026-05-06 12:18:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -109,4 +109,6 @@ threading.Thread(target=run_bot, daemon=True).start()
 # ================== تشغيل السيرفر ==================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
